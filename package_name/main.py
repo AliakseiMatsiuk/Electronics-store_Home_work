aa076dc8cb97f3f841f0431e9b4ba3c962824778
Matsiuk Aliaksei <murryaa@gmai.com>
2023-02-19 15:52:26 +0300

    home work 2

--- package_name/main.py
+++ package_name/main.py
@@ -1,21 +1,53 @@
+import csv
 class Item:
     pay_rate = 0.8
-    instances = []
+    all = []
 
-    def __init__(self, name: str, price, amount):
-        self.name = name
+    def __init__(self,name:str,price:int,amount:int):
+        self.__name = name
         self.price = price
         self.amount = amount
-        Item.instances.append(self)
+        Item.all.append(self)
 
     def calculate_total_price(self):
+        """Возвращает цену товра"""
         return self.price * self.amount
 
     def apply_discount(self):
+        """Применяет к цене скидку"""
         return self.pay_rate * self.price
 
-# item1 = Item("Смартфон", 10000, 20)
-# item2 = Item("Ноутбук", 20000, 5)
+    @classmethod
+    def instantiate_from_csv(cls):
+        """Создаёт новые экзэмпляры из csv файла"""
+        with open("items.csv", "r") as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
+            for row in reader:
+                cls(row['name'], int(row['price']), int(row['quantity']))
 
-# print(item1.calculate_total_price())
-# print(item2.calculate_total_price())
+    @property
+    def name(self):
+        """Возвращает название товра"""
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        """Контролирует длину названия товра"""
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            print(f'Exception: Длина наименования товара превышает 10 символов.')
+
+    @staticmethod
+    def is_integer(num) -> bool:
+        """Определения целого числа в методе класса"""
+        if isinstance(num, int) or (isinstance(num, float) and num % 1 == 0):
+            return True
+        return False
+
+
+#item = Item("Смартфон", 10000, 20)
+# Item.instantiate_from_csv()
+# print(len(Item.all))
+# i = Item.all[0]
+#print(Item.is_integer(5.5))
